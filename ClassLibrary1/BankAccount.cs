f148d1bf96d4dbfafa064f07c97d48cf9e149b0e
visualstudio <visualstudio@230-PC45>
2019-03-14 16:45:39 -0400

    commit7

---
+++ ClassLibrary1/BankAccount.cs
@@ -0,0 +1,78 @@
+﻿using System;
+// Name: Mandeep Kaur(730387)
+//Ramandeep Singh(732140)
+
+namespace BankAccount
+{
+    class Program
+    {
+
+    }
+    public class BankAccount
+    {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+        private BankAccount()
+        {
+        }
+        public BankAccount(String customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+
+        }
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public string DebitAmountLessThanzZeroBalanceMessage { get; private set; }
+
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if (amount > m_balance)
+            {
+                string DebitAmountExceedsBalanceMessage = null;
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
+            }
+            if (amount < 0)
+            {
+                string DebitAmountLessThanzZeroMessage = null;
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanzZeroMessage);
+            }
+            m_balance -= amount;
+        }
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+        }
+
+
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is $ {0}", ba.Balance);
+        }
+    }
+
+}
